5a6c01900268d9f29d9f108bc3f5ffe04c807e15
Abdullah <callmeab@gmail.com>
2026-01-15 14:46:07 +0500

    feat: Add Capacitor Browser plugin, implement custom back button handling on the premium page, and remove the restore purchase UI from settings.

--- src/app/tabs/premium/premium.page.ts
+++ src/app/tabs/premium/premium.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavController, IonicModule } from '@ionic/angular';
+import { NavController, IonicModule, Platform } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { arrowBackOutline } from 'ionicons/icons';
 import { MonetizationService } from '../../services/monetization.service';
@@ -13,17 +13,33 @@ import { MonetizationService } from '../../services/monetization.service';
   imports: [CommonModule, IonicModule],
 })
 export class PremiumPage implements OnInit {
+  private backButtonSubscription: any;
+
   constructor(
     private navCtrl: NavController,
-    private monetizationService: MonetizationService
+    private monetizationService: MonetizationService,
+    private platform: Platform
   ) {
     addIcons({ 'arrow-back': arrowBackOutline });
   }
 
   ngOnInit() {}
 
+  ionViewWillEnter() {
+    this.backButtonSubscription =
+      this.platform.backButton.subscribeWithPriority(10, () => {
+        this.goBack();
+      });
+  }
+
+  ionViewWillLeave() {
+    if (this.backButtonSubscription) {
+      this.backButtonSubscription.unsubscribe();
+    }
+  }
+
   goBack() {
-    this.navCtrl.back();
+    this.navCtrl.navigateBack('/tabs/setting');
   }
 
   async buyPremium() {
